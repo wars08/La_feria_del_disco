469e0804a41a66ab8ed221eb79cf798ed7669db4
wars08 <68737561+wars08@users.noreply.github.com>
2022-02-18 01:57:06 -0500

    Update APP.php

--- config/APP.php
+++ config/APP.php
@@ -3,7 +3,7 @@
 	/*----------  
 	Ruta o dominio del servidor  - Server path or domain
 	----------*/
-	const SERVERURL="https://laferiadeldisco.azurewebsites.net/La_feria_del_disco-main";
+	const SERVERURL="laferiadeldisco.azurewebsites.net/La_feria_del_disco-main";
 
 
 	/*----------  
