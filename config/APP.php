190f0e14eed83d8ee83dc476905586e29c6d3934
wars08 <68737561+wars08@users.noreply.github.com>
2022-02-18 01:47:52 -0500

    Update APP.php

--- config/APP.php
+++ config/APP.php
@@ -3,7 +3,7 @@
 	/*----------  
 	Ruta o dominio del servidor  - Server path or domain
 	----------*/
-	const SERVERURL="https://laferiadeldisco.azurewebsites.net/";
+	const SERVERURL="https://laferiadeldisco.azurewebsites.net";
 
 
 	/*----------  
