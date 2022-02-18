c404322596154731eb79a6d1864661e3c59cc202
wars08 <wilso.rojas@udla,edu.co>
2022-02-18 01:26:09 -0500

    mpc

---
+++ vistas/contenidos/es/web-index.php
@@ -0,0 +1,66 @@
+<div class="banner">
+    <div class="banner-body">
+        <h2 class="text-uppercase">Bienvenido a <?php echo COMPANY; ?></h2>
+        <p>Contamos con más de 45 años de experiencia en el mercado, ofreciendo instrumentos musicales a toda la región del Caquetá.</p>
+    </div>
+</div>
+
+<div class="container-fluid container-web-page border-bottom bg-white">
+    <h3 class="text-center text-uppercase poppins-regular font-weight-bold">Nuestros productos y servicios</h3>
+    <br>
+    
+    <div class="row"> 
+     
+     <div  class="col-12 col-sm-6 col-md-4" >
+         <a href="<?php echo SERVERURL; ?>product/1/ASC/1/" class="footer-link"> 
+            <p class="text-center">  <i class="fas fa-guitar fa-5x"></i></p>     
+            <h5 class="text-center text-uppercase poppins-regular font-weight-bold">Instrumentos musicales</h5>       
+            <p class="text-center">Contamos con una amplia variedad de instrumentos de cuerda, percusión y viento de marcadas nacionales e internacionales.  </p>
+         </a>
+     </div>
+
+    
+        
+     
+         <div class="col-12 col-sm-6 col-md-4">
+         <a href="<?php echo FACEBOOK; ?>" class="footer-link"> 
+            <p class="text-center"><i class="fas fa-boxes fa-5x"></i> </p>
+            <h5 class="text-center text-uppercase poppins-regular font-weight-bold">Accesorios y componentes</h5>
+            <p class="text-center">Tenemos a disposición todos los accesorios con precios accesibles y de excelente calidad.</p>
+         </a>
+        </div>
+     
+
+    
+        <div class="col-12 col-sm-6 col-md-4"> 
+         <a href="<?php echo FACEBOOK; ?>" class="footer-link"> 
+            <p class="text-center"><i class="fas fa-store-alt fa-5x"></i></p>
+            <h5 class="text-center text-uppercase poppins-regular font-weight-bold">Reparación y fabricación</h5>
+            <p class="text-center">Ofrecemos servicios de luteria tanto para la reparación o restauración de su instrumento. </p>
+         </a>
+        </div>
+     
+     
+    </div>
+    
+</div>
+
+
+<div class="container container-web-page">
+    <div class="row justify-content-md-center">
+        <div class="col-12 col-md-6">
+            <figure class="full-box">
+                <img src="<?php echo SERVERURL; ?>vistas/assets/img/registration.png" alt="registration_<?php echo COMPANY; ?>" class="img-fluid">
+            </figure>
+        </div>
+        <div class="w-100"></div>
+        <div class="col-12 col-md-6">
+            <h3 class="text-center text-uppercase poppins-regular font-weight-bold">Crea tu cuenta</h3>
+            <p class="text-justify">Registrate en nuestra tienda para poder realizar pedidos de productos, es muy fácil y rápido.
+            </p>
+            <p class="text-center">
+                <a href="<?php echo SERVERURL; ?>registration/" class="btn btn-primary" >Crear cuenta</a>
+            </p>
+        </div>
+    </div>
+</div>
